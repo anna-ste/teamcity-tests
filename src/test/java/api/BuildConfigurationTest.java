1a92aad318eac52041985e990c915458401b92c1
Anna Filippova <bryzhevska@MacBook-Pro-Anna.local>
2023-04-22 21:03:48 +0200

    Update test with comments

--- src/test/java/api/BuildConfigurationTest.java
+++ src/test/java/api/BuildConfigurationTest.java
@@ -8,24 +8,30 @@ import org.workshop.api.models.VcsRoot;
 import org.workshop.api.requests.CheckedRequest;
 
 
-public class BuilConfigurationTest extends BaseTest {
+public class BuildConfigurationTest extends BaseTest {
     @Test
     public void xbuildConfigurationTest() {
 
         var testData = testDataGenerator.generate();
         var checkedRequest = new CheckedRequest();
+
+        // create project
         var project = checkedRequest.createProject(testData.getNewProjectDescription());
 
+        // update test data to use created project id
         BuildType buildType = testData.getBuildType();
-        Project buildTypeProject = buildType.getProject();
-        buildType.setProject(buildTypeProject.builder().id(project.getId()).build());
+        buildType.setProject(Project.builder().id(project.getId()).build());
         VcsRoot vcsRoot = testData.getVcsRoot();
-        Project vcsRootProject = vcsRoot.getProject();
-        vcsRoot.setProject(vcsRootProject.builder().id(project.getId()).build());
+        vcsRoot.setProject(Project.builder().id(project.getId()).build());
 
+        // create vcs root
         checkedRequest.createVcsRoot(vcsRoot);
+
+        // create and run build configuration
         checkedRequest.createBuildConfiguration(buildType);
         var createdBuildType = checkedRequest.runBuildConfiguration(Build.builder().buildType(testData.getBuildType()).build());
+
+        // wait until build finished and assert build status
         var build = checkedRequest.getBuild(createdBuildType.getId());
         checkedRequest.waitUntilBuildFinished(build.getId());
         softy.assertThat(build.getStatus()).isEqualTo("SUCCESS");
